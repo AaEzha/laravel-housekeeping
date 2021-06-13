319f90ad6ce479c81bc5c1c07505688e937455b6
AaEzha <aaezha@gmail.com>
2021-06-14 00:17:01 +0700

    Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Kamar;
+use App\StatusKamar;
 use App\User;
 use Database\Seeders\RoleSeeder;
 use Illuminate\Database\Seeder;
@@ -24,6 +26,21 @@ class DatabaseSeeder extends Seeder
             ['name' => 'Admin', 'last_name' => 'Pertama', 'email' => 'admin@gmail.com', 'role_id' => '1', 'password' => bcrypt('password')],
             ['name' => 'Tamu', 'last_name' => 'Pertama', 'email' => 'tamu@gmail.com', 'role_id' => '2', 'password' => bcrypt('password')],
             ['name' => 'Menunggu', 'last_name' => '(Pending)', 'email' => 'petugas@gmail.com', 'role_id' => '3', 'password' => bcrypt('password')]
+        ]);
+
+        StatusKamar::insert([
+            ['status_kamar' => 'Terisi'],
+            ['status_kamar' => 'Ready'],
+            ['status_kamar' => 'Kosong'],
+        ]);
+
+        Kamar::insert([
+            ['nomor_kamar' => '101', 'status_kamar_id' => 1],
+            ['nomor_kamar' => '102', 'status_kamar_id' => 1],
+            ['nomor_kamar' => '103', 'status_kamar_id' => 2],
+            ['nomor_kamar' => '104', 'status_kamar_id' => 2],
+            ['nomor_kamar' => '105', 'status_kamar_id' => 3],
+            ['nomor_kamar' => '106', 'status_kamar_id' => 3],
         ]);
     }
 }
